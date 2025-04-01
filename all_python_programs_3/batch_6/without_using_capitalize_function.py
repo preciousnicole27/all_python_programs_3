ca7e5cff9ca046fcc477401189d410927b0bbd97
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 23:42:59 +0800

    make the first letter capital while the rest are in lowercase

--- all_python_programs_3/batch_6/without_using_capitalize_function.py
+++ all_python_programs_3/batch_6/without_using_capitalize_function.py
@@ -5,6 +5,7 @@ def capitalize_text(text):
     if not text:
         return text
 # convert input
+    return text[0].upper() + text[1:].lower()
 # ask for user input
 text = input("Enter a text:")
 # print the results
