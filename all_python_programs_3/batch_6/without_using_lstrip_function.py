13ea30406f48610b0347273b96bf711640d67365
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 11:58:00 +0800

    use loop to findthe first non space character

--- all_python_programs_3/batch_6/without_using_lstrip_function.py
+++ all_python_programs_3/batch_6/without_using_lstrip_function.py
@@ -1,8 +1,9 @@
 # define the function
 def remove_leading_spaces():
-# use loop
 # init index
     index = 0
+# use loop
+    while index < len(text) and text [index] == " ":
 # use slicing
 # ask for user input
 text = input("Enter a text:")
