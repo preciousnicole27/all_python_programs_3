3453dec47d53e637803992dc350dd99010efc219
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-02 02:15:44 +0800

    remove the last character of the text then use slicing to create a new string without the last character

--- all_python_programs_3/batch_7/without_using_rstrip_function.py
+++ all_python_programs_3/batch_7/without_using_rstrip_function.py
@@ -3,6 +3,7 @@ def remove_trailing_spaces(text):
 # use while loop
     while text and text[-1] == " ":
 # remove the last space
+        text = text[:-1]
 # use return
 # ask for user input
 text = input("Enter a text:")
