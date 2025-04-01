f73801d44faff94ae07f156fc468ebfcc26ebcfa
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 12:11:39 +0800

    use loop to check if the input starts with the given prefix

--- all_python_programs_3/batch_6/without_using_removeprefix_function.py
+++ all_python_programs_3/batch_6/without_using_removeprefix_function.py
@@ -2,6 +2,8 @@
 def remove_prefix(text, prefix):
 # init index
 # check if input starts with a prefix
+    while index < len(prefix) and index < len(text) and text [index]:
+        index += 1
 # use slicing
 # ask for user input
 text = input("Enter a text:")
