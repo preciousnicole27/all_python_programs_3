32fd0853d18b4d03c7a86ec15ca45113be835ffd
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 22:10:46 +0800

    use loop to go through each character in text to convert and swap cases

--- all_python_programs_3/batch_6/without_using_swapcase_function.py
+++ all_python_programs_3/batch_6/without_using_swapcase_function.py
@@ -2,6 +2,8 @@
 def swap_case(text):
 # create an empty string
 # use loop
+    for char in text:
+        result += char.upper() if char.islower() else char.lower()
 # use return
 # ask for user input
 text = input("Enter a text:")
