237adc1befd1c97019af375b072431d6321fbd63
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 21:49:50 +0800

    check text length then calculate the spaces needed

--- all_python_programs_3/batch_6/without_using_center_function.py
+++ all_python_programs_3/batch_6/without_using_center_function.py
@@ -2,6 +2,9 @@
 def center_text(text, width):
 # check text length
 # calculate spaces
+    total_spaces = max(0, width - len(text))
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
 # return text
 # ask for user input 
 text = input("Enter an input:")
