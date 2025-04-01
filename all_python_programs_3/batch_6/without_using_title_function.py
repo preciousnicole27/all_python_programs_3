c420dfae7d11a9285de80f03b4df5a002797c204
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 23:52:31 +0800

    capitalize the first letter while the rest are in lower case

--- all_python_programs_3/batch_6/without_using_title_function.py
+++ all_python_programs_3/batch_6/without_using_title_function.py
@@ -3,6 +3,7 @@ def title_cased_text(text):
 # use split
     words = text.split()
 # convert the input
+    result = [word[0].upper() + word[1:].lower() if word else "" for word in words]
 # combine words
 # ask for user input
 text = input("Enter a text:")
