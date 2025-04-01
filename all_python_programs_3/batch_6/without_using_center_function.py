79d1d8ca2f54791d1dd5f2d41636a190919f3172
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 21:51:06 +0800

    return text

--- all_python_programs_3/batch_6/without_using_center_function.py
+++ all_python_programs_3/batch_6/without_using_center_function.py
@@ -6,6 +6,7 @@ def center_text(text, width):
     left_spaces = total_spaces // 2
     right_spaces = total_spaces - left_spaces
 # return text
+    return " " * left_spaces + text + " " * right_spaces
 # ask for user input 
 text = input("Enter an input:")
 width = int(input("Enter a width:"))
