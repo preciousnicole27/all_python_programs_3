c4b32852d6e8c1a6f486a3b1661b1a86903ad824
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 21:35:37 +0800

    print the justified text

--- all_python_programs_3/batch_6/without_using_ljust_function.py
+++ all_python_programs_3/batch_6/without_using_ljust_function.py
@@ -4,9 +4,10 @@ def justified_text(text,width):
     if len(text) >= width:
         return text
 # return text
-    return text + " " * (width -len(text))
 # add spaces
+    return text + " " * (width -len(text))
 # ask for user input
 text = input("Enter a text:")
 width = int(input("Enter a width:"))
 # print results
+print(justified_text(text, width))
