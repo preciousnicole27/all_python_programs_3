b0c4607a8dfe1e3e4193ec183f9c619668564a33
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 17:49:26 +0800

    slice to compare last characters of the text with the given suffix then use return

--- all_python_programs_3/batch_6/without_using_endswith_function.py
+++ all_python_programs_3/batch_6/without_using_endswith_function.py
@@ -1,6 +1,10 @@
 # define the function
+def check_suffix(text, suffix):
 # use slicing
+    if len(suffix) > len(text):
 # use return
+        return False
+    return text[-len(suffix):] == suffix
 # ask for user input 
 text = input("Enter an input:")
 suffix = input("Enter a suffix")
