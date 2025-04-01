9a276262ab4563cc577f9d3f68924010373b90e9
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-01 21:29:14 +0800

    check text length

--- all_python_programs_3/batch_6/without_using_ljust_function.py
+++ all_python_programs_3/batch_6/without_using_ljust_function.py
@@ -1,6 +1,8 @@
 # define the function
 def justified_text(text,width):
-# check text length 
+# check text length
+    if len(text) >= width:
+        return text
 # return text
 # add spaces
 # ask for user input
