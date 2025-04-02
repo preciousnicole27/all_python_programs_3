22a5df4fa802fea5ea035664284445b885619bf2
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-02 23:59:15 +0800

    return false if the prefix is at the end

--- all_python_programs_3/batch_7/without_using_startswith_function.py
+++ all_python_programs_3/batch_7/without_using_startswith_function.py
@@ -1,7 +1,11 @@
 # define the function
 def check_prefix(text, prefix):
 # use slicing
+    if len(prefix) > len(text):
 # use return
+        return False
+    return True
 # ask for user input
 text = input("Enter a text:")
+prefix = input("Enter a prefix:")
 # print results
