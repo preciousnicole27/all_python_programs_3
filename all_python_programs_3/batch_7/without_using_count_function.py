226525482386a0c72d61cd1294c8d695239e140e
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-03 01:17:04 +0800

    loop continously through the string then check if any substring matches the slice of the string if a match is found add 1 to the count

--- all_python_programs_3/batch_7/without_using_count_function.py
+++ all_python_programs_3/batch_7/without_using_count_function.py
@@ -3,8 +3,11 @@ def count_text(text, substring):
 # init count
     count = 0
 # use loop
-# check if substring matches string
+    for i in range(len(text) - len(substring) + 1):
+# check is substring matches string
+        if text[i:i +len(substring)] == substring:
 # increment count 
+            count += 1
 # return the final count
 # ask for user input
 text = input("Enter a text:")
