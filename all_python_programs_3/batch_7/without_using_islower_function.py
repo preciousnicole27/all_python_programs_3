c82cd79f5c0d6e5cc68a55d9437734b2f4118173
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-02 23:43:18 +0800

    use loop to go through each character continously if there are any uppercase letter

--- all_python_programs_3/batch_7/without_using_islower_function.py
+++ all_python_programs_3/batch_7/without_using_islower_function.py
@@ -1,6 +1,8 @@
 # define the function
 def to_all_lowercase_letter(text):
 # use loop
+    for character in text:
+        if "A" <= char <= "Z":
 # use return
 # ask for user input
 text = input("Enter an input:")
