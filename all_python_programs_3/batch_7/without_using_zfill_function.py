334817793aa8bea4eb1c747965e3d10c4dfbfe9e
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-03 00:51:12 +0800

    define the function that will add zeroes at the beggining of the input, then check text length if longer or equal to the width, if text length is equal to the width return original

--- all_python_programs_3/batch_7/without_using_zfill_function.py
+++ all_python_programs_3/batch_7/without_using_zfill_function.py
@@ -1,6 +1,9 @@
 # define the function
+def zero_justified_text(text,width):
 # check text length
+    if len(text) >= width:
 # return text
+        return text
 # add zeroes
 # ask for user input
 text = input("Enter a text:")
