3b8df25cb8049340aa73eec1486fb137362627ad
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-02 23:15:45 +0800

    use loop to check if through each character of the text is in lowercase if not keep the original case

--- all_python_programs_3/batch_7/without_using_upper_function.py
+++ all_python_programs_3/batch_7/without_using_upper_function.py
@@ -3,6 +3,11 @@ def to_uppercase_letters(text):
 # init an empty string
 # use loop
 # check if letter is lowercase
+    for letter in text:
+        if "a" <= letter <= "z":
+            result.append(letter.upper())
+        else:
+            result.append(letter)
 # use return
 # ask for user input
 text = input("Enter a text:")
