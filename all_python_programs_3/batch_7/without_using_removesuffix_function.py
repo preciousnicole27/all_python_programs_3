ab7825d49b271657d75b9fe0c7b52e42db357d39
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-02 11:19:24 +0800

    return original text if given suffix is not found

--- all_python_programs_3/batch_7/without_using_removesuffix_function.py
+++ all_python_programs_3/batch_7/without_using_removesuffix_function.py
@@ -3,8 +3,9 @@ def remove_suffix(text, suffix):
 # check if text ends with suffix
     if text.endswith(suffix):
         return text[:-len(suffix)]
-    return text
 # use return
+    return text
+
 # ask for user input
 text = input("Enter a text:")
 suffix = ("Enter a suffix:")
