00b6b5f20f3e29e2be1a07574728d827b252993c
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-03 01:35:51 +0800

    define the function, use loop through the string then check if the substring matches current position, if it matches, return the original index if no match found return -1

--- all_python_programs_3/batch_7/without_using_index_function.py
+++ all_python_programs_3/batch_7/without_using_index_function.py
@@ -1,5 +1,9 @@
 # define function
+def index_loc(text, substring):
 # use loop
+    for i in range(len(text) - len(substring) + 1):
+        if text[i:i + len(substring)] == substring:
+            return i
 # check substring
 # use return
 # ask for user input
