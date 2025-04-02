b46bf2bfeeb892e1123815a51db8f1435096a02f
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-03 01:51:52 +0800

    define the function, then use loop to start from last index then check if substring match current position. if it matches return the current index if no match found return -1

--- all_python_programs_3/batch_7/without_using_rindex_function.py
+++ all_python_programs_3/batch_7/without_using_rindex_function.py
@@ -1,5 +1,10 @@
 # define the function
+def rindex_loc(text, substring):
 # use loop
+    for i in range(len(text) - len(substring), -1, -1):
+        if text[i:i + len(substring)] == substring:
+            return i
+    return -1
 # check substring
 # use return
 # ask for user input
