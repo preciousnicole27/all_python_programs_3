87694cafd9c21903a291d7a3ecc5029c441eb45a
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-03 01:53:13 +0800

    print the results of the defined rindex function

--- all_python_programs_3/batch_7/without_using_rindex_function.py
+++ all_python_programs_3/batch_7/without_using_rindex_function.py
@@ -1,13 +1,14 @@
 # define the function
 def rindex_loc(text, substring):
 # use loop
+# check substring
+# use return
     for i in range(len(text) - len(substring), -1, -1):
         if text[i:i + len(substring)] == substring:
             return i
     return -1
-# check substring
-# use return
 # ask for user input
 text = input("Enter a text:")
 substring = input("Enter the substring:")
 # print the result
+print(rindex_loc(text, substring))
