0efed2c8d95fd03d064e8833903cbe97ea0086e3
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-03 01:37:29 +0800

    print the result of the index

--- all_python_programs_3/batch_7/without_using_index_function.py
+++ all_python_programs_3/batch_7/without_using_index_function.py
@@ -1,12 +1,13 @@
 # define function
 def index_loc(text, substring):
 # use loop
+# check substring
+# use return
     for i in range(len(text) - len(substring) + 1):
         if text[i:i + len(substring)] == substring:
             return i
-# check substring
-# use return
 # ask for user input
 text = input("Enter a text:")
 substring = input(Enter the substring:)
 # print the results
+print(index_loc(text, substring))
