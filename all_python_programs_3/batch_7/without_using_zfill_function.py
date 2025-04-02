cc3cc5393df891b3de126dce754a6dbe06a932f8
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-03 00:52:23 +0800

    if not add zeroes then return the modfied text

--- all_python_programs_3/batch_7/without_using_zfill_function.py
+++ all_python_programs_3/batch_7/without_using_zfill_function.py
@@ -5,6 +5,7 @@ def zero_justified_text(text,width):
 # return text
         return text
 # add zeroes
+    return "0" * (width - len(text)) + text
 # ask for user input
 text = input("Enter a text:")
 # print the results
